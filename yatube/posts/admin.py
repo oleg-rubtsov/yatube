91ce6c37acdaf00b88e5eaa98766e4c628d85e56
Oleg Rubtsov <oleg.rubtsov99@gmail.com>
2022-01-27 20:26:10 +0300

    new

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,11 +3,7 @@ from django.contrib import admin
 from .models import Post, Group, Follow, Comment
 
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ("pk", "text", "pub_date", "author", "group")
-    search_fields = ("text",)
-    list_filter = ("pub_date",)
-    empty_value_display = "-пусто-"
+
 
 
 class GroupAdmin(admin.ModelAdmin):
@@ -30,7 +26,18 @@ class CommentAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "author", "group")
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
+    empty_value_display = "-пусто-"
+
+
 admin.site.register(Post, PostAdmin)
+
+
+
+
 admin.site.register(Group, GroupAdmin)
 admin.site.register(Follow, FollowAdmin)
 admin.site.register(Comment, CommentAdmin)
